bc474424f66cef579ed5b1a0838cfbc5bb45cbd3
uoooohub <uoooohub@protonmail.com>
2020-12-10 06:47:20 +0900

    update

--- src/cgi-bin/api.py
+++ src/cgi-bin/api.py
@@ -30,7 +30,7 @@ try:
         c.execute("select * from articles")
         d = []
         for row in c:
-            d.append(row.fetchone())
+            d.append(row)
         conn.close()
         print(json.dumps(_res(200,msg="ok",data=d)))
     else:
