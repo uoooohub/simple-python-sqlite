ebc7dca4d31abb99b8981ab06906458a92050ae7
uoooohub <uoooohub@protonmail.com>
2020-12-10 04:53:19 +0900

    update

--- init.sh
+++ init.sh
@@ -4,4 +4,6 @@ sed -e "s/MY_WEB_SERVICE_NAME/${service_name}/g" tmp/docker-compose.yml > docker
 sed -i -e "s/MY_DB_SERVICE_NAME/${service_name}/g" docker-compose.yml
 cp tmp/build/python/Dockerfile python/Dockerfile
 
+chmod 755 src/cgi-bin/api.py
+
 docker-compose up
