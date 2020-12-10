5d23b3f04b1012fee3a21a53db633400664181b9
uoooohub <uoooohub@protonmail.com>
2020-12-10 23:34:14 +0900

    update

--- init.sh
+++ init.sh
@@ -1,12 +1,13 @@
-service_web_name=$1
-service_db_name=$2
+python_version=$1
+service_web_name=$2
+service_db_name=$3
 
 sed -e "s/MY_WEB_SERVICE_NAME/${service_web_name}/g" tmp/docker-compose.yml > docker-compose.yml
 sed -i -e "s/MY_DB_SERVICE_NAME/${service_db_name}/g" docker-compose.yml
-cp tmp/build/python/Dockerfile python/Dockerfile
+sed -i -e "s/PYTHON_VERSION/${python_version}/g" docker-compose.yml
+sed -e "s/PYTHON_VERSION/${python_version}/g" tmp/build/python/Dockerfile > python/Dockerfile
 
 chmod 755 src/cgi-bin/api.py
 
-docker-compose run $service_web_name python main.py
-
+docker-compose run $service_db_name python ./main.py
 docker-compose up
--- memo.sh
+++ memo.sh
@@ -1,5 +1,5 @@
 git clone https://github.com/uoooohub/simple-python-sqlite.git . \
 && bash create_requirements.sh ./python 'requests' 'bs4' \
-&& bash init.sh 'web123' 'db123'
+&& bash init.sh 'python:3.8-slim' 'web123' 'db123'
 
 
