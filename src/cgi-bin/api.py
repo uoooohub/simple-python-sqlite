7ef1fe0e743c9350552016949c349b97d478c6ca
uoooohub <uoooohub@protonmail.com>
2020-12-10 22:06:07 +0900

    update

--- src/cgi-bin/api.py
+++ src/cgi-bin/api.py
@@ -26,7 +26,6 @@ try:
     if apiName.value == 'get':
         import sqlite3
         conn = sqlite3.connect('/code/data/example.db')
-        conn.row_factory = sqlite3.Row
         c = conn.cursor()
         c.execute("select * from articles")
         d = []
