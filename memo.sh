5fb3c17f8cacf1e7bf01ff108e2be185011e3126
uoooohub <uoooohub@protonmail.com>
2020-12-10 23:44:15 +0900

    update

--- memo.sh
+++ memo.sh
@@ -1,5 +1,6 @@
-git clone https://github.com/uoooohub/simple-python-sqlite.git . \
-&& bash create_requirements.sh ./python 'requests' 'bs4' \
+root_path=${pwd}
+&& git clone https://github.com/uoooohub/simple-python-sqlite.git . \
+&& bash create_requirements.sh $root_path/python 'requests' 'bs4' \
 && bash init.sh 'python:3.8-slim' 'web123' 'db123'
 
 
